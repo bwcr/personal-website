afd0bfc4a28e0d1bcba102c4eb6eb8d08daad4c3
bwcr_ <bayuwicaksono98@gmail.com>
2023-05-30 14:26:38 +0700

    feat: font import

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,12 @@ import "../styles/fonts/remixicon.css";
 import { theme } from "../themes";
 import { SkipNavLink } from "@chakra-ui/skip-nav";
 import { Analytics } from "@vercel/analytics/react";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 
 export function Chakra({ cookies, children }) {
   // b) Pass `colorModeManager` prop
