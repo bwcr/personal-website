aeb2ea9cd449ba20e4db4aebbd542d7babcdbfec
bwcr_ <bayuwicaksono98@gmail.com>
2023-05-30 21:30:56 +0700

    feat: google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,15 @@ import "../styles/fonts/remixicon.css";
 import { theme } from "../themes";
 import { SkipNavLink } from "@chakra-ui/skip-nav";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import { GTM_ID, pageview } from "../lib/gtag";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 import "@fontsource/inter/900.css";
+import { useRouter } from "next/router";
 
 export function Chakra({ cookies, children }) {
   // b) Pass `colorModeManager` prop
@@ -30,9 +33,30 @@ export function Chakra({ cookies, children }) {
 }
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   return (
     <Chakra cookies={pageProps.cookies}>
       <SkipNavLink>Skip to content</SkipNavLink>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </Chakra>
--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { theme } from "../themes";
+import { GTM_ID } from "../lib/gtag";
 
 export default class Document extends NextDocument {
   render() {
@@ -11,6 +12,14 @@ export default class Document extends NextDocument {
         <Head />
         <Html lang="en">
           <body>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              ></iframe>
+            </noscript>
             {/* 👇 Here's the script */}
             <ColorModeScript initialColorMode={theme.config.initialColorMode} />
             <Main />
